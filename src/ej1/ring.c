36103e71f66b00e2c7a97d3b5108529d7fbb07bd
Nico Romero <nromero@dc.uba.ar>
2024-04-11 17:42:25 -0300

    Create ring.c

---
+++ src/ej1/ring.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char **argv)
+{	
+	int start, status, pid, n;
+	int buffer[1];
+
+	if (argc != 4){ printf("Uso: anillo <n> <c> <s> \n"); exit(0);}
+    
+    /* Parsing of arguments */
+  	/* TO COMPLETE */
+    printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer[0], start);
+    
+   	/* You should start programming from here... */
+}
